2f6aa1cde4ec8f65db2bd2dadcb08c0f5612e5a3
pedrorribeiro6 <pedro.r.ribeiro6@aluno.senai.br>
2026-03-11 09:47:30 -0300

    atualizando parte 2

--- calculadora_imc_estruturada.php
+++ calculadora_imc_estruturada.php
@@ -12,7 +12,7 @@ echo  "O IMC de Pedro é: $imc" ;
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Calcular IMC</title>
+        <title>Calculadora IMC</title>
         <style>
             body{
                 background-image: url("backgroundkk.webp");

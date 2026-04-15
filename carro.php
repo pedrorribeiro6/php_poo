de2aef60907d0a8188c860c2a5c1dcbe8f6e07e0
pedrorribeiro6 <pedro.r.ribeiro6@aluno.senai.br>
2026-04-15 16:48:09 -0300

    commit
    progresso do dia

---
+++ carro.php
@@ -0,0 +1,28 @@
+<?php
+class Carro {
+    // 🚩 RISCO: Atributo público permite valores ilegais
+    public $modelo;
+    private $velocidade;
+
+    public function __construct($modelo, $velocidade) {
+        $this->modelo = $modelo;
+        $this->velocidade = $velocidade;
+    }
+     public function getVelocidade(){
+        return $this->velocidade;
+    }
+
+    public function setVelocidade($novaVelocidade){
+
+    }
+}
+// --- TESTE DO VEÍCULO ---
+$meuCarro = new Carro("Senai-Mobile", 0);
+
+// O desastre: alteração direta sem validação
+$meuCarro->velocidade = 5000; // Velocidade de foguete?
+$meuCarro->velocidade = -60;   // Carro andando no tempo?
+
+echo "Modelo: " . $meuCarro->modelo . "<br>";
+echo "Velocidade atual: " . $meuCarro->velocidade . " km/h";
+?>
